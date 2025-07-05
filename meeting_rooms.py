3ee950430fe646914f6fd04a06977f659a61d214
BTR <thilakraj1301@gmail.com>
2025-07-04 18:49:49 -0700

    Create meeting_rooms.py

---
+++ meeting_rooms.py
@@ -0,0 +1,18 @@
+# Time complexity: O(n log n)
+# Space complexity: O(n)
+
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x:x[0])
+        rooms = [intervals[0][1]]
+
+        for start,end in intervals[1:]:
+            if start >= rooms[0]:
+                heapq.heappop(rooms)
+            heapq.heappush(rooms,end)
+        
+        return len(rooms)
+
+    
